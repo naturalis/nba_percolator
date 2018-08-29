b117b404c9f8a4d8dafd4a9cdd1b64c749d789bf
Joep Vermaat <joep@joep-i.nl>
2018-08-29 14:05:33 +0200

    Refactor, improved documentation

--- ppdb_nba/ppdb_nba.py
+++ ppdb_nba/ppdb_nba.py
@@ -15,7 +15,6 @@ import yaml
 from elasticsearch import Elasticsearch
 
 from pony.orm import db_session
-from .schema import *
 
 FORMAT = u'%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
@@ -61,6 +60,136 @@ except:
     logger.fatal(msg)
     sys.exit(msg)
 
+class Nsrtaxa_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Nsrtaxa_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Nsrmedia_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Nsrmedia_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Crsspecimen_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Crsspecimen_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Crsmedia_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Crsmedia_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Brahmsspecimen_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Brahmsspecimen_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Brahmsmedia_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Brahmsmedia_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Xenocantospecimen_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Xenocantospecimen_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Xenocantomedia_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Xenocantomedia_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Coltaxa_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Coltaxa_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Waarnemingspecimen_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Waarnemingspecimen_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Waarnemingmedia_current(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
+
+
+class Waarnemingmedia_import(db.Entity):
+    rec = Optional(Json)
+    hash = Optional(str, index=True)
+    datum = Required(datetime, sql_default='now()')
 
 
 def open_deltafile(action='new', index='unknown'):
