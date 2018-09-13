3611bb00b604812d42007e99e05726d989d9b806
Joep Vermaat <joep@joep-i.nl>
2018-09-13 15:35:58 +0200

    raw query

--- ppdb_nba/ppdb_nba.py
+++ ppdb_nba/ppdb_nba.py
@@ -353,7 +353,8 @@ def list_impacted(sourceconfig, scientificnamegroup):
     currenttable = globals()[table.capitalize() + '_current']
 
     jsonsql = 'rec->\'identifications\' @> \'[{"scientificName":{"scientificNameGroup":"%s"}}]\'' % (scientificnamegroup)
-    items = db.Brahmsspecimen_current.select(lambda p: raw_sql(jsonsql))
+    items = currenttable.select(lambda p: raw_sql(jsonsql))
+
     if (len(items)):
         logger.info("Found {number} records in {source} with scientificNameGroup={namegroup}".format(number=len(items), source=table.capitalize(), namegroup=scientificnamegroup))
         return items
