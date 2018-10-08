190f713bba0cd29265bebcc1b9553a70112862ca
Joep Vermaat <joep@joep-i.nl>
2018-10-08 17:01:37 +0200

    database schema

--- ppdb_nba/schema.py
+++ ppdb_nba/schema.py
@@ -9,28 +9,24 @@ from pony.orm import Database, Optional, Json, Required, raw_sql
 ppdb = Database()
 
 class Nsrtaxa_import(ppdb.Entity):
-    _table_ = ('nsrtaxa', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Nsrtaxa_current(ppdb.Entity):
-    _table_ = ('nsrtaxa', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Nsrmedia_import(ppdb.Entity):
-    _table_ = ('nsrmedia', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Nsrmedia_current(ppdb.Entity):
-    _table_ = ('nsrmedia', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
@@ -49,112 +45,96 @@ class Crsspecimen_current(ppdb.Entity):
 
 
 class Crsmedia_import(ppdb.Entity):
-    _table_ = ('crsmedia', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Crsmedia_current(ppdb.Entity):
-    _table_ = ('crsmedia', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Brahmsspecimen_import(ppdb.Entity):
-    _table_ = ('brahmsspecimen', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Brahmsspecimen_current(ppdb.Entity):
-    _table_ = ('brahmsspecimen', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Brahmsmedia_import(ppdb.Entity):
-    _table_ = ('brahmsmedia', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Brahmsmedia_current(ppdb.Entity):
-    _table_ = ('brahmsmedia', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Xenocantospecimen_import(ppdb.Entity):
-    _table_ = ('xcspecimen', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Xenocantospecimen_current(ppdb.Entity):
-    _table_ = ('xcspecimen', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Xenocantomedia_import(ppdb.Entity):
-    _table_ = ('xcmedia', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Xenocantomedia_current(ppdb.Entity):
-    _table_ = ('xcmedia', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Coltaxa_import(ppdb.Entity):
-    _table_ = ('coltaxa', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Coltaxa_current(ppdb.Entity):
-    _table_ = ('coltaxa', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Waarnemingspecimen_import(ppdb.Entity):
-    _table_ = ('wrnspecimen', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Waarnemingspecimen_current(ppdb.Entity):
-    _table_ = ('wrnspecimen', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Waarnemingmedia_current(ppdb.Entity):
-    _table_ = ('wrnmedia', 'current')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
 
 
 class Waarnemingmedia_import(ppdb.Entity):
-    _table_ = ('wrnmedia', 'import')
     rec = Optional(Json)
     hash = Optional(str, index=True)
     datum = Required(datetime, sql_default='now()')
